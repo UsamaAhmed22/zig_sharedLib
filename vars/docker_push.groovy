d73f98c59e02c64fe76bc8c8b0b8bf01d2feaecb
UsamaAhmed22 <134758643+UsamaAhmed22@users.noreply.github.com>
2026-01-06 20:40:20 +0500

    Fixed docker push

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
-def call(String Project, String ImageTag, String dockerhubuser){
+def call(String Project, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
       sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
   }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+  sh "docker push ${dockerhubuser}/${Project}"
 }
